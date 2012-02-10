5ff167e579b5eaa14d26a779b3d7bce847552684
Toni Cebrián <ancechu@gmail.com>
2012-02-10 12:36:37 +0100

    Minimal viable version working

--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,17 @@
 module Main where
 
 import System.Environment
+import System.Directory
+import System.FilePath.Posix
 import NoLit
       
+
+createFile :: TangledFile -> IO ()
+createFile (TangledFile filename content) = do
+    createDirectoryIfMissing True $ takeDirectory filename
+    writeFile filename content
+    
 main = do
     args <- getArgs
     nwFile <- readFile (args !! 0)
-    putStrLn "Hola"
+    mapM createFile $ tangle nwFile
